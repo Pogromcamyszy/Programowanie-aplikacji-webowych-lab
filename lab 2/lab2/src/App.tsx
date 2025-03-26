320ce2a5a4b8e4408018c5e8b15d0a9449343d94
Pogromcamyszy <mateusz.para@microsoft.wsei.edu.pl>
2025-03-26 11:33:51 +0100

    lab 2 fixed mentioned mistakes by Teacher

--- lab 2/lab2/src/App.tsx
+++ lab 2/lab2/src/App.tsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { IProject, Project } from './Project/Project.tsx';
+import { ProjectManager } from './ProjectMeneger/ProjectMeneger.tsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const projectManager = new ProjectManager();
+
+  const [projects, setProjects] = useState<IProject[]>([]);
+  const [projectName, setProjectName] = useState<string>('');
+  const [projectDescription, setProjectDescription] = useState<string>('');
+  const [isEdited, setIsEdited] = useState<boolean>(false);
+  const [editedId, setEditedId] = useState<number>(0);
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProjectName(e.target.value);
+  };
+
+  const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProjectDescription(e.target.value);
+  };
+
+  const addProject = () => {
+    const project = new Project(projectName, projectDescription);
+    projectManager.addProject(project);
+    setProjectName('');
+    setProjectDescription('');
+    setProjects(projectManager.getProjects()); // Update the list after adding
+  };
+
+  const editProject = () => {
+    projectManager.editProject(editedId, projectName, projectDescription);
+    setIsEdited(false);
+    setEditedId(0);
+    setProjectName('');
+    setProjectDescription('');
+  }
+
+  const goToEdit = (id: number) => {
+    const project = projectManager.getProject(id);
+    setProjectName(project.name);
+    setProjectDescription(project.description);
+    setIsEdited(true);
+    setEditedId(id);
+  }
+
+  const deleteProject = (id: number) => {
+    projectManager.deleteProject(id);
+    setProjects(projectManager.getProjects());
+  }
+  
+  const cancelEdit = () => {
+    setIsEdited(false);
+    setEditedId(0);
+  }
+
+  useEffect(() => {
+    setProjects(projectManager.getProjects());
+  }, [projects]);  
 
   return (
+    <div>
+      <h1>Project List</h1>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>
+            {project.id} <strong>{project.name}</strong>: {project.description} 
+            {!isEdited && (
+              <>
+              <button onClick={() => goToEdit(project.id)}>Edit</button>
+              <button onClick={() => deleteProject(project.id)}>Delete</button>
+              </>
+              ) }
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        placeholder="Project Name"
+        value={projectName}
+        onChange={handleNameChange}
+      />
+      <input
+        type="text"
+        placeholder="Project Description"
+        value={projectDescription}
+        onChange={handleDescriptionChange}
+      />
+     {!isEdited &&(
+      <>
+      <button onClick={addProject}>Add Project</button>
+      </>)}
+
+    {isEdited &&(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <button onClick={() => {editProject()}}>Edit project</button>
+    <button onClick={() => {cancelEdit()}}>Cancel</button>
     </>
-  )
+    )}
+  
+  </div>);
 }
 
-export default App
+export default App;
---
+++ lab 2/lab2/src/ProjectMeneger/ProjectMeneger.tsx
@@ -0,0 +1,39 @@
+import { Project, IProject } from './Project';
+
+export class ProjectManager {
+  projects: Project[];
+
+  constructor() {
+    this.projects = JSON.parse(localStorage.getItem('projects') || '[]');
+  }
+
+  addProject(project: IProject) {
+    this.projects.push(project);
+    localStorage.setItem('projects', JSON.stringify(this.projects));
+  }
+
+  editProject(id:number , name: string, description: string) {
+    const index = this.projects.findIndex((p) => p.id === id);
+    console.log(index);
+    const project:IProject = this.projects[index];
+    project.name = name;
+    project.description = description;
+    this.projects[index] = project;
+    localStorage.setItem('projects', JSON.stringify(this.projects));
+  }
+
+  deleteProject(id: number) {
+    const index = this.projects.findIndex((p) => p.id === id);
+    this.projects.splice(index, 1);
+    localStorage.setItem('projects', JSON.stringify(this.projects));
+  };
+
+  getProject(id: number) {
+    const project = this.projects.find((p) => p.id === id);
+    return project;
+  }
+
+  getProjects() {
+    return this.projects;
+  }
+}
