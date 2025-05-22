a07c53af4e76a9e2d3e26cce90d63d3ba46f1e2c
technomancer <bozios11@gmail.com>
2025-05-22 20:55:25 +0200

    lab 4 made api for login

--- corrected/lab 4/kanban/frontend/src/App.tsx
+++ corrected/lab 4/kanban/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import Projects from "./Projects/ProjectsComponent";
 import Stories from "./Stories/StoriesComponent"
 import Task from "./Task/TasksComponent";
+import Login from "./Login/LoginComponent";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
            <Route path="/" element={<Projects/>}/>
            <Route path="/:projectId/story" element={<Stories />} />
            <Route path="/:projectId/story/:storyId/task" element={<Task />} />
-           <Route path="/:projectId/story/:storyId/task/:taskId" element={<Task />} />
+           <Route path="/login" element={<Login/>} />
            </Routes>
         </Router>
     </>
--- corrected/lab 4/kanban/server/main.js
+++ corrected/lab 4/kanban/server/main.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
-const jwt = require('jsonwebtoken');
-
 const express = require('express')
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = 3000
 
 app.use(express.json());
+app.use(cookieParser());
 
 const SECRET_KEY = process.env.JWT_SECRET_KEY;
 // frontend decode the jwt before request -> if expired, post to refresh token endpoint
@@ -30,21 +31,64 @@ app.listen(port, () => {
 
 app.post('/login', (req, res) => {
     const { userfirstName, password } = req.body;
-    userList.forEach(user => {
-        if (user.firstName === userfirstName && user.password === password) {
-            res.cookie('token', generateRefreshToken(user), {
-                httpOnly: true,
-                secure: false,
-                maxAge: 7 * 24 * 60 * 60 * 1000,
-                sameSite: 'lax'
-            });
-            res.status(200).json({ message: 'Login successful', token: generateToken(user) });
-            return;
-        }
-    })
 
-    res.status(401).json({ message: 'Invalid userfirstName or password' });
+    const user = userList.find(user => user.firstName === userfirstName && user.password === password);
+
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid userfirstName or password' });
+    }
+
+    res.cookie('token', generateRefreshToken(user), {
+        httpOnly: true,
+        secure: false,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        sameSite: 'lax'
+    });
+
+    return res.status(200).json({ message: 'Login successful', token: generateToken(user) });
 });
+
+app.get('/user', authenticateToken, (req, res) => {
+   const token = req.headers['authorization']?.split(' ')[1];
+   const decoded = jwt.decode(token);
+   res.status(200).json({ user: decoded });
+});
+
+app.post('/refresh-token', (req, res) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+
+        const user = userList.find(user => user.id === decoded.id);
+        if (!user) {
+            return res.status(401).json({ message: 'User not found' });
+        }
+
+        const newToken = generateToken(user);
+        res.status(200).json({ token: newToken });
+    });
+});
+
+function authenticateToken(req, res, next) {
+    const token = req.headers['authorization']?.split(' ')[1];
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        req.user = user;
+        next();
+    });
+}
 
 function generateToken(user) {
     return jwt.sign({ id: user.id, firstName: user.firstName, lastName: user.lastName, role: user.role }, SECRET_KEY, {
